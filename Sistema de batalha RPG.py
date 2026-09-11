13e45f5ceb1ed202e8953a0b464dafb0cca046b4
Pedro Henrique Caballero Dacunha <82623003+ZedroDacunha@users.noreply.github.com>
2026-09-11 15:45:41 -0400

    completo

--- Sistema de batalha RPG.py
+++ Sistema de batalha RPG.py
@@ -1,5 +1,5 @@
 class Personagem:
-    def __init__(self, nome, vida, forca, defesa):
+    def __init__(self, nome: str, vida: float, forca: int, defesa: int):
         self.nome = nome
         self.vida = vida
         self.forca = forca
@@ -7,16 +7,15 @@ class Personagem:
     # TODO: Implemente atacar(self, outro_personagem) aqui
 # ---- CÓDIGO DE TESTE ---
     def atacar(self, outro_personagem):
-        dano = forca/2
-        
-        if outro_personagem.defesa >= self.forca:
-            print(f"{self.nome} atacou {outro_personagem.nome}, mas não causou dano.")
-        elif outro_personagem.defesa < self.forca:
+        dano = self.forca
+        if outro_personagem.vida <= 0:
+            print(f"{outro_personagem.nome} já está morto. Não é possível atacá-lo.")
+        elif dano >= outro_personagem.vida and outro_personagem.vida > 0:
+            print(f"{self.nome} matou {outro_personagem.nome} causando {dano} de dano")
+            outro_personagem.vida = 0
+        elif outro_personagem.defesa < self.forca and outro_personagem.vida > 0:
             print(f"{self.nome} acertou o ataque, {outro_personagem.nome} perdeu {dano} PV")
             outro_personagem.vida = outro_personagem.vida - dano
-        elif outro_personagem.vida <= 0:
-            print(f"{inimigo.nome} já está morto. Não é possível atacá-lo.")
-            return
 
 p1 = Personagem("Guerreiro", vida=50, forca=15, defesa=5)
 p2 = Personagem("Mago", vida=20, forca=10, defesa=2)
